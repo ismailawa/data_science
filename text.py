09438f612ab6474d495c5b392d3c2613294f5b99
ismailawa aliyu <ismailawa.aliyu@gmail.com>
2024-01-25 12:33:23 +0100

    initial commit

---
+++ text.py
@@ -0,0 +1,21 @@
+
+c = False
+d = False
+
+
+print("c", end=" => ")
+print(c)
+
+print("d", end=" => ")
+print(d)
+
+print("c and d", end=" = ")
+print(c and d)
+
+print("not c", end=" = ")
+print(not c)
+
+print("not d", end=" = ")
+print(not d)
+
+
